333b97652be5e1fc9cc4ba09adb52141115e2f96
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-04-26 20:33:44 +0300

    Add TimeOverException.

---
+++ src/main/java/com/restaurant/votingsystem/util/exception/TimeOverException.java
@@ -0,0 +1,7 @@
+package com.restaurant.votingsystem.util.exception;
+
+public class TimeOverException extends RuntimeException {
+    public TimeOverException(String message) {
+        super(message);
+    }
+}
