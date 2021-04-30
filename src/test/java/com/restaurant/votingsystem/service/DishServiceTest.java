f3f83953700009259424f95af0b03e7da7f5c41d
Oleksii Riahuzov <riahuzovoleksii@gmail.com>
2021-04-30 16:31:43 +0300

    Add DishServiceTest.

--- src/test/java/com/restaurant/votingsystem/service/DishServiceTest.java
+++ src/test/java/com/restaurant/votingsystem/service/DishServiceTest.java
@@ -1,17 +1,16 @@
 package com.restaurant.votingsystem.service;
 
 import com.restaurant.votingsystem.model.Dish;
+import com.restaurant.votingsystem.util.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
-import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static com.restaurant.votingsystem.DishTestData.*;
+import static com.restaurant.votingsystem.data.DishTestData.*;
+import static com.restaurant.votingsystem.data.MenuTestData.MENU1_ID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DishServiceTest extends AbstractServiceTest {
 
@@ -20,46 +19,39 @@ class DishServiceTest extends AbstractServiceTest {
 
     @Test
     void create() {
-        Dish created = dishService.create(getNew());
-        int newId = created.id();
         Dish newDish = getNew();
-        newDish.setId(newId);
-        DISH_MATCHER.assertMatch(created, newDish);
-        DISH_MATCHER.assertMatch(dishService.get(newId), newDish);
+        Dish created = dishService.create(getNew(), MENU1_ID);
+        newDish.setId(created.id());
+        assertEquals(created, newDish);
     }
 
     @Test
     void delete() {
-        dishService.delete(DISH1_ID);
-        assertThrows(NoSuchElementException.class, () -> dishService.get(DISH1_ID));
+        dishService.delete(DISH1_ID, MENU1_ID);
+        assertThrows(NotFoundException.class, () -> dishService.get(DISH1_ID, MENU1_ID));
     }
 
     @Test
     void deleteNotFound() {
-        assertThrows(EmptyResultDataAccessException.class, () -> dishService.delete(NOT_FOUND));
+        assertThrows(NotFoundException.class, () -> dishService.delete(NOT_FOUND, MENU1_ID));
     }
 
     @Test
     void get() {
-        Dish dish = dishService.get(DISH2_ID);
-        DISH_MATCHER.assertMatch(dish, dish2);
-    }
-
-    @Test
-    void getAllByDate() {
-        DISH_MATCHER.assertMatch(dishService.getAllByDate(LocalDate.of(2021, Month.JANUARY, 16)), dish2);
+        Dish actual = dishService.get(DISH1_ID, MENU1_ID);
+        DISH_MATCHER.assertMatch(actual, DISH_1);
     }
 
     @Test
     void update() {
         Dish updated = getUpdated();
-        dishService.update(updated);
-        DISH_MATCHER.assertMatch(dishService.get(DISH2_ID), getUpdated());
+        dishService.update(updated, MENU1_ID);
+        DISH_MATCHER.assertMatch(dishService.get(DISH1_ID, MENU1_ID), updated);
     }
 
     @Test
     void getAll() {
-        List<Dish> all = dishService.getAll();
-        DISH_MATCHER.assertMatch(all, dish1, dish2);
+        List<Dish> actual = dishService.getAll(MENU1_ID);
+        DISH_MATCHER.assertMatch(actual, List.of(DISH_1, DISH_2, DISH_3));
     }
 }
